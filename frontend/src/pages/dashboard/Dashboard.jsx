b8e26c27064e3ff51c91e38fe2734739ef7e2143
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-23 20:02:15 +0530

    fix: removed caching for useQuery in dashboard page

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { DashboardContainer } from './styles';
 import { PageTitle } from '../../common/styled-components';
-import { useGetReadingsQuery } from '../../slices/readingApiSlice';
+import { useLazyGetReadingsQuery } from '../../slices/readingApiSlice';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import ReadingList from '../../components/readingList/ReadingList';
 import { isArrayEmpty } from '../../utils';
 
 const Dashboard = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  const { data: readings, isLoading } = useGetReadingsQuery();
+  const [trigger, { data: readings, isLoading }] = useLazyGetReadingsQuery();
+
+  //usLazyQuery with trigger will call the api irrespective of cache.
+  useEffect(() => {
+    trigger();
+  }, [trigger])
 
   if (isLoading) {
     return <LoadingSpinner />
