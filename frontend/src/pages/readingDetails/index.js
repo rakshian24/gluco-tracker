603409e29a086a9d332b194ed8a8a1d080911580
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-27 18:27:47 +0530

    feat: added fallback for reading details page

--- frontend/src/pages/readingDetails/index.js
+++ frontend/src/pages/readingDetails/index.js
@@ -3,11 +3,16 @@ import { useGetReadingQuery } from '../../slices/readingApiSlice';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { useParams } from 'react-router-dom';
 import { PageTitle } from '../../common/styled-components';
-import { getFormattedDate, isArrayEmpty } from '../../utils';
+import { getFormattedDate, getSvgWidth, isArrayEmpty } from '../../utils';
 import ReadingDetailsCard from '../../components/readingDetailsCard';
+import FallBackScreen from '../../components/fallbackScreen';
+import { ROUTES } from '../../constants';
+import NoDataFoundSvgComponent from '../../components/NoDataFoundSvgComponent';
+import { useWindowSize } from '../../hooks/useWindowResize';
 
 const ReadingDetails = () => {
   const { date } = useParams();
+  const [screenWidth] = useWindowSize();
   const { data: readings, isLoading } = useGetReadingQuery({ date });
 
   if (isLoading) {
@@ -17,9 +22,16 @@ const ReadingDetails = () => {
   return (
     <>
       <PageTitle>Readings for {getFormattedDate(date)}</PageTitle>
-      {!isArrayEmpty(readings) && readings.map(reading => {
+      {!isArrayEmpty(readings) ? readings.map(reading => {
         return <ReadingDetailsCard key={reading._id} readingObj={reading} />
-      })}
+      }) : <FallBackScreen
+        title={"No data found for this date."}
+        subtitle={"Please add some readings."}
+        showCta={true}
+        ctaLink={ROUTES.CREATE_READING}
+        ctaText={"Add Reading"}
+        svgComponent={<NoDataFoundSvgComponent width={getSvgWidth(screenWidth)} />}
+      />}
     </>
   )
 }
